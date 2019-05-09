73d552d021da27e21adedc3f6874901dd9c2661a
DeviNur <41379185+DeviNur@users.noreply.github.com>
2019-05-09 15:32:17 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id9508007_sertifikasi","santi123","id9508007_santi") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id7899377_santi","santi123","id7899377_sertifikasi") or die ("could not connect database");
 ?>
